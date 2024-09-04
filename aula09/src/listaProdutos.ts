d936144600768d69cff4d2161f5c6c459785df23
Julio-CRodrigues <juliocesarccr29@gmail.com>
2024-09-04 10:39:50 -0300

    Criando um tipo especifico e disponibilizando ele atraves de arquivo types

--- aula09/src/listaProdutos.ts
+++ aula09/src/listaProdutos.ts
@@ -1,7 +1,7 @@
+import { TipoListaObj } from "./types";
 
 //Criar uma lista de objetos de Produtos Eletrônicos
-export const listaProdutos = [
-    {
+export const listaProdutos:TipoListaObj[] = [    {
         id: 1,
         nome: "Smartphone",
         preco: 1500.00,
